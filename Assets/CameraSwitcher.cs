07940afb35cd9b4c44d60d9ad74649f76e85e61b
Marshall verboekend <Marshalllyron@gmail.com>
2024-05-13 13:50:55 +0200

    camera switch

---
+++ Assets/CameraSwitcher.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    public GameObject firstPerson;
+    public GameObject thirdPerson;
+    public GameObject hoodView;
+    public GameObject backView;
+    
+    void Start()
+    {
+    
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab) && firstPerson.activeSelf == true)
+        {
+            firstPerson.SetActive(false);
+            thirdPerson.SetActive(true);
+            Debug.Log("To third-person");
+        }
+        else if (Input.GetKeyDown(KeyCode.Tab) && thirdPerson.activeSelf == true)
+        {
+            thirdPerson.SetActive(false);
+            hoodView.SetActive(true);
+            Debug.Log("To hood-view");
+        }
+        else if (Input.GetKeyDown(KeyCode.Tab) && hoodView.activeSelf == true)
+        {
+            hoodView.SetActive(false);
+            backView.SetActive(true);
+            Debug.Log("To back-view");
+        }
+        else if (Input.GetKeyDown(KeyCode.Tab) && backView.activeSelf == true)
+        {
+            backView.SetActive(false);
+            firstPerson.SetActive(true);
+            Debug.Log("To first-person");
+        }
+    }
+}

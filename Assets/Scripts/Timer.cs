793dafbd1e2cf93cebb4787694157f583b064351
wbvanderbroek <wbvanderbroek@st.noorderpoort.nl>
2024-05-16 19:03:51 +0200

    better checkpoint texture + register finish

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -9,6 +9,7 @@ public class Timer : MonoBehaviour
     [SerializeField] float RemainingTime;
     float ElapsedTime;
     bool isLastCheckpointReached = false;
+    [SerializeField] private TextMeshProUGUI goText;
 
     void Update()
     {
@@ -23,6 +24,10 @@ public class Timer : MonoBehaviour
             }
             else
             {
+                if (GetComponent<CarController>().allowDrive == false)
+                {
+                    StartCoroutine(ShowGoUI());
+                }
                 GetComponent<CarController>().allowDrive = true;
                 ElapsedTime += Time.deltaTime;
                 int minutes = Mathf.FloorToInt(ElapsedTime / 60);
@@ -37,5 +42,11 @@ public class Timer : MonoBehaviour
     {
         isLastCheckpointReached = true;
         Debug.Log("Last checkpoint reached. Timer stopped.");
+    }
+    private IEnumerator ShowGoUI()
+    {
+        goText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        goText.gameObject.SetActive(false);
     }
 }
